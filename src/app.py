2d2799723e806be1b89dee839689e03c22b83266
Gabriella Conde <conde.gabriella.r@gmail.com>
2025-09-28 02:29:14 -0400

    Integrate Depth-Anything-V2

--- src/app.py
+++ src/app.py
@@ -1,6 +1,40 @@
-import time
-import cv2
+import sys, os
+sys.path.append("/home/eli/Desktop/hackgt25/Depth-Anything-V2/depth_anything_v2")
+
+import time, cv2, torch
+import numpy as np
+import matplotlib
 from picamera2 import Picamera2
+
+da_path = "/home/eli/Desktop/hackgt25/Depth-Anything-V2"
+if os.path.isdir(da_path) and da_path not in sys.path:
+    sys.path.insert(0, da_path)
+
+from depth_anything_v2.dpt import DepthAnythingV2
+
+
+
+# ==================== Depth Anything Setup ==================== #
+
+DEVICE = 'cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu'
+
+model_configs = {
+    'vits': {'encoder': 'vits', 'features': 64, 'out_channels': [48, 96, 192, 384]},
+    'vitb': {'encoder': 'vitb', 'features': 128, 'out_channels': [96, 192, 384, 768]},
+    'vitl': {'encoder': 'vitl', 'features': 256, 'out_channels': [256, 512, 1024, 1024]},
+    'vitg': {'encoder': 'vitg', 'features': 384, 'out_channels': [1536, 1536, 1536, 1536]}
+}
+
+# using small model
+encoder ='vits'
+
+# configure the model
+model = DepthAnythingV2(**model_configs[encoder])
+model.load_state_dict(torch.load(f'../../Depth-Anything-V2/checkpoints/depth_anything_v2_{encoder}.pth', map_location='cpu'))
+model = model.to(DEVICE).eval()
+
+
+# ==================== Rasberry Pi Camera Setup ==================== #
 
 # initialize camera
 camera = Picamera2()
@@ -13,7 +47,8 @@ camera.start()
 # allow camera time to stabilize
 time.sleep(2)
 
-# capture images
+# ==================== Image Processing ==================== #
+
 for i in range(2):  # do 2 images for demo purposes
     # capture frame as numpy arrary
     frame = camera.capture_array("main")
@@ -25,7 +60,19 @@ for i in range(2):  # do 2 images for demo purposes
     # success_flag, buffer = cv2.imencode('.jpg', frame)
 
     # use to write file for testing
-    # cv2.imwrite(f"image{i}.jpg", frame)
+    cv2.imwrite(f"image{i}.jpg", frame)
+
+    # produce depth map from image (HxW in numpy)
+    depth = model.infer_image(frame_rgb) # HxW raw depth map in numpy
+
+    # THE FOLLOWING LINES FOR VISUALIZING ONLY
+    # scale depth map to range 0 to 255
+    depth = (depth - depth.min()) / (depth.max() - depth.min()) * 255.0
+    depth = depth.astype(np.uint8)
+    # drop alpha channel, convert from rgb to bgr, conver to uint8 for viewing
+    cmap = matplotlib.colormaps.get_cmap('Spectral')
+    depth = (cmap(depth)[:, :, :3] * 255)[:, :, ::-1].astype(np.uint8)
+    cv2.imwrite(f"image{i}_depth.jpg", depth)
 
     # wait for new plate to enter frame - 10 seconds
-    time.sleep(10)
+    time.sleep(2)
